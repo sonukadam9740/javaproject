a37c0674dc07adce29fe686cf8d6b600797c28db
sonu kadam <sonalikadam8088@gmail.com>
2023-06-18 14:57:53 +0530

    sonu kadam

---
+++ pg/hello.java
@@ -0,0 +1,18 @@
+package pg;
+
+public class switch {
+
+	public static void main(String[] args) {
+		char(choice)="A"
+		switch(choice)
+		{
+		case a :system.out.print("excellent");
+		case b :system.out.print("good");
+		
+		case c :system.out.print("POOR");
+	
+		}
+		
+	}
+
+}
